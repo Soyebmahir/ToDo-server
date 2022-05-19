a24c42aae2804ad4fcbb6edd5e28fb3793f68183
Soyeb <tssoyeb@gmail.com>
2022-05-19 06:16:13 +0600

    delete a data

--- index.js
+++ index.js
@@ -33,7 +33,13 @@ try{
         const result = await taskCollection.insertOne(task);
         res.send(result);
     })
-  
+    // delete one item 
+    app.delete('/task/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+    })
 
 } 
 finally{
